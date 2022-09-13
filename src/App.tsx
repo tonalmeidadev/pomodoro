464516bc1631368cf38c98d52798e155b5e29b21
Ton Almeida <ton@tonalmeida.dev>
2022-09-13 16:58:52 -0300

    updating temp

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ export default function App() {
   return (
     <div className='flex flex-col gap-2 items-center justify-center w-full max-w-sm m-auto py-10 px-5'>
       <PomodoroTimer
-        pomodoroTime={1800}
-        shortRestTime={300}
+        pomodoroTime={10}
+        shortRestTime={5}
         longRestTime={5}
         cycles={4}
       />
